35661aeae230e19b41b0299802351f12ca0de953
Florian Gustin <florian.gustin.business@gmail.com>
2022-03-10 16:48:49 +0100

    feat : pending

--- lib/ui/pages/chat_page.dart
+++ lib/ui/pages/chat_page.dart
@@ -15,45 +15,91 @@ class ChatPage extends StatefulWidget {
 class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: ListView.builder(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-      itemBuilder: (context, index){
-      return Bubble();
-    }, itemCount: fakeItems.length,));
+    return Scaffold(body: SafeArea(
+      child: ListView.builder(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        itemBuilder: (context, index){
+        return fakeItems[index];
+      }, itemCount: fakeItems.length,),
+    ));
   }
 }
 
 
 final fakeItems = [
-  "Bonjour"
+  BubbleWidget(),
+  BubbleWidget(),
+  BubbleWidget(userType: UserType.client,),
+  BubbleWidget(),
+  ProposalWidget()
 ];
 
 
-class Bubble extends StatefulWidget {
-  const Bubble({Key? key}) : super(key: key);
 
-  @override
-  _BubbleState createState() => _BubbleState();
+
+
+
+enum UserType{
+  client,
+  bot,
+  pro
 }
 
-class _BubbleState extends State<Bubble> {
+class BubbleWidget extends StatefulWidget {
+  const BubbleWidget({Key? key, this.userType = UserType.bot}) : super(key: key);
+
+  final UserType userType;
+
+  @override
+  _BubbleWidgetState createState() => _BubbleWidgetState();
+}
+
+class _BubbleWidgetState extends State<BubbleWidget> {
+
+
+  dynamic applyOnUserType(dynamic client, dynamic bot, dynamic pro) {
+    switch(widget.userType){
+      case UserType.client:
+        return client;
+      case UserType.bot:
+        return bot;
+      case UserType.pro:
+        return pro;
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      Icon(Icons.chat_outlined, color: AppColors().black,),
+    return Row(
+      mainAxisAlignment: applyOnUserType(MainAxisAlignment.end,MainAxisAlignment.start, MainAxisAlignment.start),
+      children: [
+      if(widget.userType != UserType.client)
+        Icon(applyOnUserType(null, Icons.person, Icons.personal_video), color: const AppColors().black, size: 40,),
       Card(
-        color: AppColors().brown,
+        color: applyOnUserType(const AppColors().lightGreen, const AppColors().brown, const AppColors().lightBlue),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(0),
+            topLeft: applyOnUserType(Radius.circular(15), Radius.circular(0), Radius.circular(0)),
             topRight: Radius.circular(15),
             bottomLeft: Radius.circular(15),
-            bottomRight: Radius.circular(15),
+            bottomRight:  applyOnUserType(Radius.circular(0), Radius.circular(15), Radius.circular(15)),
           )
         ),
         child: Container(
-          padding: EdgeInsets.all(10),
-          child: Text('okok', style: context.theme.primaryTextTheme.headline4?.copyWith(color: AppColors().white ),),),)
+          padding: const EdgeInsets.all(10),
+          child: Text('okok', style: context.theme.primaryTextTheme.headline4?.copyWith(color: const AppColors().white ),),),)
     ],);
+  }
+}
+
+
+
+class ProposalWidget extends StatelessWidget {
+  const ProposalWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
