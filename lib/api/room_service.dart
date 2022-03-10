4f3d3a1ee0135ac7be9b723127e6d1d7295c00ae
Nathan <fnathan@hotmail.fr>
2022-03-10 23:58:47 +0100

    with rooms api call

---
+++ lib/api/room_service.dart
@@ -0,0 +1,12 @@
+import 'package:brumaire_frontend/models/room.dart';
+import 'package:dio/dio.dart';
+
+const String url = "https://brumaire.nospy.fr";
+
+Future<List<Room>> getRooms() async {
+  var dio = Dio();
+  final response = await dio.get(url + '/rooms');
+  return List<Room>.from(response.data.map((model) => Room.fromJson(model)));
+}
+
+
--- lib/ui/pages/all_conversations_page.dart
+++ lib/ui/pages/all_conversations_page.dart
@@ -1,3 +1,5 @@
+import 'package:brumaire_frontend/api/room_service.dart';
+import 'package:brumaire_frontend/models/room.dart';
 import 'package:brumaire_frontend/ui/theme/i_theme_styles.dart';
 import 'package:brumaire_frontend/ui/theme/styles.dart';
 import 'package:flutter/material.dart';
@@ -31,49 +33,71 @@ class AllConversationsPage extends StatelessWidget {
           //     ),
           //   ),
           // ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      "assets/images/chat_bot_icone.png",
-                      height: 40,
-                      width: 40,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text("All conversations",
-                          style: context.theme.primaryTextTheme.headline2
-                              ?.copyWith(color: const AppColors().black)),
-                    )
-                  ],
-                ),
-                Expanded(
-                  child: ListView.builder(
-                      itemCount: 3,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: ListTile(
-                            onTap: () => print("clic"),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5.0)),
-                            tileColor: Colors.grey,
-                            title: Text(
-                                "Conversation bot " + (index + 1).toString(),
-                                textAlign: TextAlign.center),
-                            trailing: const Icon(
-                              Icons.chevron_right,
-                              color: Colors.black,
-                            ),
-                          ),
-                        );
-                      }),
-                ),
-              ],
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/images/chat_bot_icone.png",
+                        height: 40,
+                        width: 40,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("All conversations",
+                            style: context.theme.primaryTextTheme.headline2
+                                ?.copyWith(color: AppColors().black)),
+                      )
+                    ],
+                  ),
+                  Expanded(
+                    child: FutureBuilder<List<Room>>(
+                        future: getRooms(),
+                        builder:
+                            (BuildContext context, AsyncSnapshot snapshot) {
+                          if (!snapshot.hasData) {
+                            return const Center(child: CircularProgressIndicator());
+                          }
+                          List<Room> conversations = snapshot.data;
+                          if (snapshot.data.length == 0) {
+                            return Center(
+                              child: Text(
+                                "There is currently no conversation",
+                                style: context.theme.primaryTextTheme.headline2,
+                              ),
+                            );
+                          } else {
+                            return ListView.builder(
+                                itemCount: conversations.length,
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: ListTile(
+                                      onTap: () => print("clic"),
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(5.0)),
+                                      tileColor: Colors.grey,
+                                      title: Text(
+                                          "Conversation bot " +
+                                              (index + 1).toString(),
+                                          textAlign: TextAlign.center),
+                                      trailing: const Icon(
+                                        Icons.chevron_right,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  );
+                                });
+                          }
+                        }),
+                  )
+                ],
+              ),
             ),
           ),
         ],
