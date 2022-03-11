e86959751c6e27e0cbc08fb0f2797e203726e3a1
Nathan <fnathan@hotmail.fr>
2022-03-11 01:17:13 +0100

    with room_conversation

--- lib/api/room_service.dart
+++ lib/api/room_service.dart
@@ -1,12 +1,17 @@
 import 'package:brumaire_frontend/models/room.dart';
+import 'package:brumaire_frontend/models/room_conversation.dart';
 import 'package:dio/dio.dart';
 
 const String url = "https://brumaire.nospy.fr";
 
 Future<List<Room>> getRooms() async {
   var dio = Dio();
-  final response = await dio.get(url + '/rooms');
+  final response = await dio.get("$url/rooms");
   return List<Room>.from(response.data.map((model) => Room.fromJson(model)));
 }
 
-
+Future<RoomConversation> getConversationByRoomId(int id) async {
+  var dio = Dio();
+  final response = await dio.get("$url/conversations/rooms/$id");
+  return RoomConversation.fromJson(response.data);
+}
